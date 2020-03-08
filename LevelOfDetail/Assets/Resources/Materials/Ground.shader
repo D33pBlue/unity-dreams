a7c21ec96f9b91b437bedfc8d3e6012878b8948f
d33pblue <francescob994@protonmail.com>
2020-03-08 14:00:55 +0100

    Level of Detail

---
+++ LevelOfDetail/Assets/Resources/Materials/Ground.shader
@@ -0,0 +1,71 @@
+﻿Shader "Unlit/Ground"
+{
+    Properties
+    {
+        _MainTex ("Texture", 2D) = "white" {}
+        _ColGrass("Grass Color",Color) = (0,0.7,0.2)
+        _ColRoad("Road Color",Color) = (0,0,0)
+        _RoadSize("Road Size",Range(0.0,10.0)) = 1.0
+    }
+    SubShader
+    {
+        Tags { "RenderType"="Opaque" }
+        LOD 100
+
+        Pass
+        {
+            CGPROGRAM
+            #pragma vertex vert
+            #pragma fragment frag
+            // make fog work
+            #pragma multi_compile_fog
+
+            #include "UnityCG.cginc"
+
+            struct appdata
+            {
+                float4 vertex : POSITION;
+                float2 uv : TEXCOORD0;
+            };
+
+            struct v2f
+            {
+                float2 uv : TEXCOORD0;
+                float4 worldCoord : WORLDCOORD;
+                UNITY_FOG_COORDS(1)
+                float4 vertex : SV_POSITION;
+            };
+
+            sampler2D _MainTex;
+            float4 _MainTex_ST;
+            float4 _ColGrass;
+            float4 _ColRoad;
+            float _RoadSize;
+
+            v2f vert (appdata v)
+            {
+                v2f o;
+                o.vertex = UnityObjectToClipPos(v.vertex);
+                o.worldCoord = mul(unity_ObjectToWorld, v.vertex);
+                o.uv = TRANSFORM_TEX(v.uv, _MainTex);
+                UNITY_TRANSFER_FOG(o,o.vertex);
+                return o;
+            }
+
+            fixed4 frag (v2f i) : SV_Target
+            {
+                // sample the texture
+                fixed4 col = tex2D(_MainTex, i.uv);
+                if(abs(i.worldCoord.x)<_RoadSize){
+                    col = col*_ColRoad;
+                }else{
+                    col = col*_ColGrass;
+                }
+                // apply fog
+                UNITY_APPLY_FOG(i.fogCoord, col);
+                return col;
+            }
+            ENDCG
+        }
+    }
+}
---
+++ LevelOfDetail/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class PlayerController : MonoBehaviour
+{
+
+	public Transform PlayerBody;
+	public Transform PlayerCamera;
+	public Transform SpawnPointInside;
+	public Transform SpawnPointOutside;
+	public CharacterController controller;
+	public Transform GroundCheck;
+	public LayerMask groundMask;
+	float groundDistance = 0.4f;
+	float mouseSensitivity = 200;
+	float speed = 8;
+	float gravity = -9.81f;
+	float jumpHeight = 3;
+	Vector3 velocity;
+	float xRotation = 0;
+	bool isGrounded;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    void OnControllerColliderHit(ControllerColliderHit hit)
+	{
+		if(hit.gameObject.name=="contactpt"){
+			controller.enabled = false;
+			PlayerBody.position = SpawnPointInside.position;
+			controller.enabled = true;
+		}
+		if(hit.gameObject.name=="contactpt2"){
+			controller.enabled = false;
+			PlayerBody.position = SpawnPointOutside.position;
+			controller.enabled = true;
+		}
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+    	isGrounded = Physics.CheckSphere(GroundCheck.position,
+    			groundDistance,groundMask);
+    	if(isGrounded&&velocity.y<0){
+    		velocity.y = -2;
+    	}
+    	// update rotation
+        float mouseX = Input.GetAxis("Mouse X")*mouseSensitivity*Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y")*mouseSensitivity*Time.deltaTime;
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation,-45.0f,45.0f);
+        PlayerCamera.localRotation = Quaternion.Euler(xRotation,0,0);
+        PlayerBody.Rotate(0,mouseX,0);
+        // update motion
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+        Vector3 move = PlayerBody.right*x+PlayerBody.forward*z;
+        controller.Move(move*speed*Time.deltaTime);
+        //update jump
+        if(Input.GetButtonDown("Jump")&&isGrounded){
+        	velocity.y = Mathf.Sqrt(jumpHeight*(-1)*gravity);
+        }
+        // update gravity
+        velocity.y += gravity*Time.deltaTime;
+        controller.Move(velocity*Time.deltaTime);
+    }
+}
